db2024b05b13802ce25aea8c63bbfb79c7c4217c
Dmitr-iy <KOSHELD@INBOX.RU>
2023-03-12 20:51:48 +0500

    install commit

--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -8,16 +8,9 @@ from .models import Product
 
 
 class ProductsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Product
-    # # Поле, которое будет использоваться для сортировки объектов
     ordering = 'name'
-    # queryset = Product.objects.order_by(
-    #     'name'
-    # )
     template_name = 'products.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
 
     def get_context_data(self, **kwargs):
@@ -27,9 +20,6 @@ class ProductsList(ListView):
         return context
 
 class ProductDetail(DetailView):
-    # Модель всё та же, но мы хотим получать информацию по отдельному товару
     model = Product
-    # Используем другой шаблон — product.html
     template_name = 'product.html'
-    # Название объекта, в котором будет выбранный пользователем продукт
     context_object_name = 'product'
